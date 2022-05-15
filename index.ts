546ca4665ee65f1d1b8cbc2da4df0b4d10259a3e
Guillaume de Cagny <gdc@gdc.dev>
2022-05-15 02:36:59 +0200

    fix split by lines : the last one was not returned

--- index.ts
+++ index.ts
@@ -1,29 +1,56 @@
-import { OperatorFunction, pipe } from "rxjs";
+import { Observable, OperatorFunction, pipe } from "rxjs";
 import { Buffer } from 'buffer';
 
-import { map, toArray, scan, pluck, mergeMap } from "rxjs/operators"
+import { map, toArray, scan, mergeMap } from "rxjs/operators"
 
 
 export function waitForAllData(): OperatorFunction<Buffer, Buffer>
 { 
     return pipe(
+        map(x => Buffer.from(x)),
         toArray(),
         map((bufferList: Buffer[]) => Buffer.concat(bufferList))
     )
 }
 
 export function decode(encoding: string): OperatorFunction<Buffer, string> {
-    return map((o: Buffer) => o.toString(encoding));
+    return map((o: Buffer) => Buffer.from(o).toString(encoding));
 }
+
+interface LineSplits {
+    unfinish: string
+    lines: string[]
+}
+function pluckLines() {
+    return function(source: Observable<LineSplits>): Observable<string[]> {
+      return new Observable(subscriber => {
+        let lastVal: string;
+        source.subscribe({
+          next(value) {
+            lastVal = value.unfinish
+            subscriber.next(value.lines);
+          },
+          error(error) {
+            subscriber.error(error);
+          },
+          complete() {
+            subscriber.next([lastVal]);
+            subscriber.complete();
+          }
+        })
+      });
+    }
+  }
+  
 
 export function splitByRegex(rex: RegExp): OperatorFunction<string, string> {
     return pipe(
         scan( ({unfinish, lines}, chunk) => {
             const newAcc = unfinish + chunk
             const newlines = newAcc.split(rex)
-            return {unfinish: lines[lines.length-1], lines: newlines.slice(0,newlines.length-1)}
+            return {unfinish: newlines[newlines.length-1], lines: newlines.slice(0,newlines.length-1)}
         }, {unfinish: '', lines: [] as string[]}),
-        pluck('lines'),
+        pluckLines(),
         mergeMap(x=>x)
     )
 }
